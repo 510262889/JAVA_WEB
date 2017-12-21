8e463b15f5e275e6a4296fbf9db9bbf0580f7ce8
ykzhu <510262889@qq.com>
2017-12-21 15:13:00 +0800

    添加:添加一些常用的工具类

--- src/com/filter/CoreFilter.java
+++ src/com/filter/CoreFilter.java
@@ -1,6 +1,7 @@
 package com.filter;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -12,8 +13,14 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.PropertyConfigurator;
+
+
 public class CoreFilter implements Filter {
 
+	private static Log log = LogFactory.getLog( CoreFilter.class );
 	@Override
 	public void destroy() {
 		
@@ -27,7 +34,13 @@ public class CoreFilter implements Filter {
 	}
 
 	@Override
-	public void init(FilterConfig config) throws ServletException {
+	public void init( FilterConfig config ) throws ServletException {
+		// 初始化常用路径
+		initPath( config );
+	}
+	
+	// 初始化常用路径
+	private void initPath( FilterConfig config ){
 		ServletContext context = config.getServletContext();
 		// 应用路径
 		String app_path = context.getContextPath();
@@ -42,5 +55,6 @@ public class CoreFilter implements Filter {
 		context.setAttribute( "CSS_PATH" , css_path);
 		context.setAttribute( "JS_PATH"  , js_path);
 	}
+
 
 }
